b973203e9ec33df7098070395450d44ea1999544
GABRIEL Alex <alex.gabriel@univ-lorraine.fr>
2021-07-16 18:06:53 +0200

    update the pages

--- data/js/script.js
+++ data/js/script.js
@@ -55,3 +55,18 @@ function toggleCheckbox (element) {
         document.getElementById(element.id+"s").innerHTML = "OFF"; 
     }
 }
+
+(function () {
+	
+	$('#menubtn').on('click',function () {
+		if($('#menuBody').val() == "none" || $('#menuBody').val() == "") {
+			$('#menuBody').val('flex').removeClass('mobilehide')
+		}else if($('#menuBody').val() == "flex") {
+			$('#menuBody').val('none').addClass('mobilehide')
+		}
+		//console.log('click')
+	})
+	
+	console.log('script')
+	
+})
