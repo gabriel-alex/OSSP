c482c01b0f8b7dfdadad40a2c9463fbd045e32ca
GABRIEL Alex <alex.gabriel@univ-lorraine.fr>
2021-08-30 11:32:24 +0200

    remove useless file

--- src/credentials-default.cpp
+++
@@ -1,7 +0,0 @@
-#include <Arduino.h>
-
-class hotspot{
-    String SSID = "SSID name";
-    String password = "123456789";
-     
-};
--- src/credentials.cpp
+++
@@ -1,7 +0,0 @@
-#include <Arduino.h>
-
-class hotspot{
-    const char *SSID = "HUAWEI P smart 2019";
-    const char *password = "61547849cad7";
-     
-};
